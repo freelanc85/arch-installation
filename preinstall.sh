875c46b743ce0d703467bd18b198db7034b8cf7d
fsimchen <felipe.simchen@gmail.com>
2020-09-08 16:22:12 -0300

    commit

--- preinstall.sh
+++ preinstall.sh
@@ -14,11 +14,10 @@ timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 #curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
-reflector --latest 50 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 200 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-
-echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm btrfs-progs
+#echo -e "\nInstalling prereqs...\n$HR"
+#pacman -S --noconfirm btrfs-progs
 
 echo "-------------------------------------------------"
 echo "-------select your disk to format----------------"
@@ -72,3 +71,43 @@ echo "--------------------------------------"
 pacstrap /mnt base base-devel linux linux-firmware vim nano sudo --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
+
+# swapfile
+truncate -s 0 /swap/swapfile
+chattr +C /swap/swapfile
+btrfs property set swap/swapfile compression none
+dd if=/dev/zero of=/swap/swapfile bs=1G count=2 status=progress
+chmod 600 /swap/swapfile
+mkswap /swap/swapfile
+swapon /swap/swapfile
+echo '/swap/swapfile none swap defaults 0 0' >> /etc/fstab
+
+echo "--------------------------------------"
+echo "-- Bootloader Installation  --"
+echo "--------------------------------------"
+pacman -S grub grub-btrfs --noconfirm --needed
+
+sed -i 's/MODULES=()/MODULES=(btrfs)/g' /etc/mkinitcpio.conf
+
+mkinitcpio -p linux
+grub-install --target=i386-pc ${DISK}
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager dhclient --noconfirm --needed
+systemctl enable --now NetworkManager
+
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root user: "
+passwd root
+
+exit
+umount -R /mnt
+
+echo "--------------------------------------"
+echo "--   SYSTEM READY FOR FIRST BOOT    --"
+echo "--------------------------------------"
