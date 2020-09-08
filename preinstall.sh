52f55b01aef7cc9d5891547a02a1f770375ca17f
fsimchen <felipe.simchen@gmail.com>
2020-09-08 14:01:25 -0300

    commit

--- preinstall.sh
+++ preinstall.sh
@@ -32,15 +32,10 @@ echo "--------------------------------------"
 
 # disk prep
 sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
-# create partitions
-sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
-
-# set partition types
-sgdisk -t 1:ef00 ${DISK}
-sgdisk -t 2:8300 ${DISK}
+# Back up my original with: sfdisk -d /dev/sda > sda.dump
+# Restore
+sfdisk /dev/sda < sfdisk.dump
 
 # label partitions
 sgdisk -c 1:"UEFISYS" ${DISK}
