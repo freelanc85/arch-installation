ec9df55ab5170d00d6c4761536cc2e7fae0de785
fsimchen <felipe.simchen@gmail.com>
2020-09-11 03:41:03 -0300

    commit

--- functions.sh
+++ functions.sh
@@ -19,32 +19,32 @@ function preInstallSetup {
 
     # make filesystems
     echo -e "\nCreating Filesystems...\n"
-    mkfs.ext4 -L "BOOT" "${DISK}1"
-    mkfs.btrfs -f -L "ROOT" "${DISK}2"
+    #mkfs.ext4 -L "BOOT" "${DISK}1"
+    mkfs.btrfs -f -L "ROOT" "${DISK}1"
 
-    # mount target
-    mount "${DISK}2" /mnt
+    # create btrfs subvolumes
+    mount "${DISK}1" /mnt
     btrfs su cr /mnt/@
     btrfs su cr /mnt/@home
     btrfs su cr /mnt/@var
-    btrfs su cr /mnt/@srv
-    btrfs su cr /mnt/@opt
     btrfs su cr /mnt/@tmp
     btrfs su cr /mnt/@swap
     btrfs su cr /mnt/@.snapshots
     umount /mnt
-    mount -o noatime,compress=lzo,space_cache,subvol=@ "${DISK}2" /mnt
-    mkdir /mnt/{boot,home,var,srv,opt,tmp,swap,.snapshots}
-    mount -o noatime,compress=lzo,space_cache,subvol=@home "${DISK}2" /mnt/home
-    mount -o noatime,compress=lzo,space_cache,subvol=@srv "${DISK}2" /mnt/srv
-    mount -o noatime,compress=lzo,space_cache,subvol=@tmp "${DISK}2" /mnt/tmp
-    mount -o noatime,compress=lzo,space_cache,subvol=@opt "${DISK}2" /mnt/opt
-    mount -o noatime,compress=lzo,space_cache,subvol=@.snapshots "${DISK}2" /mnt/.snapshots
+    
+    # mount btrfs subvolumes
+    mount -o noatime,compress=lzo,space_cache,subvol=@ "${DISK}1" /mnt
+    mkdir /mnt/{home,.snapshots,tmp,var,swap}
+    mount -o noatime,compress=lzo,space_cache,subvol=@home "${DISK}1" /mnt/home
+    mount -o noatime,compress=lzo,space_cache,subvol=@.snapshots "${DISK}1" /mnt/.snapshots
+    #mount -o noatime,compress=lzo,space_cache,subvol=@tmp "${DISK}1" /mnt/tmp
+    mount -o nodatacow,subvol=@tmp "${DISK}1" /mnt/tmp
+    mount -o nodatacow,subvol=@var "${DISK}1" /mnt/var
+    mount -o nodatacow,subvol=@swap "${DISK}1" /mnt/swap
+    #mount "${DISK}1" /mnt/boot
 
-    # swap
-    mount -o nodatacow,subvol=@swap "${DISK}2" /mnt/swap
-    mount -o nodatacow,subvol=@var "${DISK}2" /mnt/var
-    mount "${DISK}1" /mnt/boot
+    chattr +C /mnt/tmp/
+    chattr +C /mnt/var/
 
     echo -e "\n--------------------------------------"
     echo "----- Arch Install on Main Drive -----"
@@ -343,6 +343,8 @@ function installSoftwareAur {
         'pnmixer'                   # System tray volume control
         'xfce4-mixer'
         'nitrogen'
+        'timeshift'
+        'timeshift-autosnap'
         
         # MEDIA ---------------------------------------------------------------
 
@@ -448,4 +450,3 @@ function finalSetup {
     ###############################################################################
     "
 }
-
