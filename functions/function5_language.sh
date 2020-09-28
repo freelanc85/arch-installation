253d9b04155fcd95219df35ce5f59a20b74f80ce
fsimchen <felipe.simchen@gmail.com>
2020-09-28 23:53:42 +0000

    fix

--- functions/function5_language.sh
+++ functions/function5_language.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function language {
-    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-    hwclock --systohc
-    sed -i "s/^#${LOCALE}/${LOCALE}/" /etc/locale.gen
-    locale-gen
-    echo LANG=$LANG >> /etc/locale.conf
-    echo -e "KEYMAP=${KEYMAP}\nFONT=ter-v32b\n" >> /etc/vconsole.conf
-    systemctl enable systemd-timesyncd
-    timedatectl --no-ask-password set-ntp 1
-    localectl --no-ask-password set-locale LANG="${LANG}" LC_TIME="${LANG}"
+    sudo ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    sudo hwclock --systohc
+    sudo sed -i "s/^#${LOCALE}/${LOCALE}/" /etc/locale.gen
+    sudo locale-gen
+    echo LANG=$LANG | sudo tee /etc/locale.conf
+    echo -e "KEYMAP=${KEYMAP}\nFONT=ter-v32b\n" | sudo tee /etc/vconsole.conf
+    sudo systemctl enable systemd-timesyncd
+    sudo timedatectl --no-ask-password set-ntp 1
+    sudo localectl --no-ask-password set-locale LANG="${LANG}" LC_TIME="${LANG}"
 }
