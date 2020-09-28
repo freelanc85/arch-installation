a813ab2d83516c22c12fed0cb01149ec814d8129
fsimchen <felipe.simchen@gmail.com>
2020-09-28 02:50:34 -0300

    andontie-aur

--- functions.sh
+++ functions.sh
@@ -207,7 +207,14 @@ function installSoftwareAur {
     yay -S --noconfirm chaotic-keyring
 
     echo '' | sudo tee -a /etc/pacman.conf
+    echo '[andontie-aur]' | sudo tee -a /etc/pacman.conf
+    echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
+    echo 'Server = https://aur.andontie.net/$arch' | sudo tee -a /etc/pacman.conf
+    echo '' | sudo tee -a /etc/pacman.conf
+
+    echo '' | sudo tee -a /etc/pacman.conf
     echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
+    echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
     echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
     echo '' | sudo tee -a /etc/pacman.conf
     
