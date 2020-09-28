4a349e98ec39f90f247a8b3a38fd70e5390fca93
fsimchen <felipe.simchen@gmail.com>
2020-09-28 02:54:47 -0300

    code fix

--- functions.sh
+++ functions.sh
@@ -211,8 +211,6 @@ function installSoftwareAur {
     echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
     echo 'Server = https://aur.andontie.net/$arch' | sudo tee -a /etc/pacman.conf
     echo '' | sudo tee -a /etc/pacman.conf
-
-    echo '' | sudo tee -a /etc/pacman.conf
     echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
     echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
     echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
